96f5286944a2f441b638982a05f5694a0955530f
harshadsindhav <harshad.sindhav@gmail.com>
2018-05-15 16:07:15 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+
+@RestController()
+@RequestMapping("test")
+class Test {
+
+    @RequestMapping("/")
+    public String test() {
+        return "Hello World!!";
+    }
+
+}
